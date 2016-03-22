724cbbdea3ab949548f0be0ebbadf387fc348627
SybilleCaffiau <sybille.caffiau@imag.fr>
2016-03-22 18:52:04 +0100

    corrections de coquilles
    - suppression des lignes mis en commentaire
    - exécutable pour la transformation du fichier en entrée corrigé
    - nom du fichier de sortie corrigé dans le message de fin

--- Traitement/modelEditionOmissionEvt.sh
+++ Traitement/modelEditionOmissionEvt.sh
@@ -1,5 +1,3 @@
-#/usr/bash
-
 #Ce fichier ne prend en compte que les cas d'omission de tâche dans les evenements
 echo "Début du processus de création du modèle des possibles (étape 4)"
 
@@ -26,31 +24,18 @@ echo "Modèle d'Edition avec les omission d'Evt"
 fstprint --isymbols=$label --osymbols=$label modeleEditionOmissionEvtModif.fst
 
 #------le modèle de tâches
-#pré-traitement pour éviter des transitions qui n'ont pas de sémantique (et qui vont des transitions non valides vis à vis du MdT 
-#echo "automate du modèle de tâches (.fst) ? "
-#read mdt
-
-
+#pré-traitement pour avoir un modele avec que des acceptors
 echo "automate du modèle de tâches (.txt) ? "
 read mdt
 
-./myapp $mdt testfinal.txt
+./formate $mdt testfinal.txt
 
-#fstrmepsilon $mdt | fstarcsort > MdT_test_papier4.fst
-#echo "après rmepsilon"
-#fstprint --isymbols=$label --osymbols=$label MdT_test_papier4.fst
 
-#fstdeterminize MdT_test_papier4.fst | fstarcsort > MdT_test_papier3.fst
 fstcompile --isymbols=$label --acceptor testfinal.txt | fstarcsort > modelTrace.fst
 
 echo "automate du MdT"
 
 fstprint --isymbols=$label --osymbols=$label modelTrace.fst
-
-#fstminimize  MdT_test_papier3.fst | fstarcsort > MdT_test_papier2.fst
-
-#echo "après minimize"
-#fstprint --isymbols=$label --osymbols=$label MdT_test_papier2.fst
 
 #le modèle d'edition du MdT
 fstcompose modeleEditionOmissionEvtModif.fst modelTrace.fst | fstarcsort > Model_possible.fst
@@ -68,9 +53,7 @@ fstcompile --isymbols=$label --acceptor $scenario | fstarcsort > Scenario.fst
 
 fstcompose Scenario.fst Model_possible.fst  | fstarcsort > Resultat.fst
 
-#fstrmepsilon Resultat.fst Res.fst
 echo "Resultat"
-#fstprint --isymbols=$label --osymbols=$label Res.fst
 fstprint --isymbols=$label --osymbols=$label Resultat.fst
 	
-echo "Fin du processus de création du modèle des chemins possibles par omissions (étape 4) -L'automate se nomme Res.fst"
+echo "Fin du processus de création du modèle des chemins possibles par omissions (étape 4) -L'automate se nomme Resultat.fst"
